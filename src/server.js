94afd1e2a0eef0e2553634f2af1176a2094c51b2
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-09-18 19:09:14 +0700

    add hapijs, nodemon, and nanoid

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({ port: 9000, host: "localhost" });
+
+  await server.start();
+  console.log(`Server running on port ${server.info.uri}`);
+};
+
+init();
