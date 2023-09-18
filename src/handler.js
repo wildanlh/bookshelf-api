2e06ae35cfe6c28b29a6f0ea18b992181b80fd14
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-09-18 23:59:55 +0700

    update filter search

--- src/handler.js
+++ src/handler.js
@@ -77,18 +77,39 @@ const addBookHandler = (request, h) => {
 };
 
 const getAllBooksHandler = (request, h) => {
+  const { name, reading, finished } = request.query;
+  let bookFilter = [...books];
+
+  if (name !== undefined) {
+    bookFilter = bookFilter.filter((book) =>
+      book.name.toLowerCase().includes(name.toLowerCase())
+    );
+  }
+
+  if (reading !== undefined) {
+    bookFilter = bookFilter.filter(
+      (book) => book.reading === !!Number(reading)
+    );
+  }
+
+  if (finished !== undefined) {
+    bookFilter = bookFilter.filter(
+      (book) => book.finished === !!Number(finished)
+    );
+  }
+
   const response = h.response({
     status: "success",
     data: {
-      books: books.map((book) => ({
+      books: bookFilter.map((book) => ({
         id: book.id,
         name: book.name,
         publisher: book.publisher,
       })),
     },
   });
-  response.code(200);
 
+  response.code(200);
   return response;
 };
 
