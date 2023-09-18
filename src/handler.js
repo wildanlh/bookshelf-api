27d38ec858e04dbd5db951bd6c330e73bdfe7120
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-09-18 20:31:28 +0700

    add addBookHandler

---
+++ src/handler.js
@@ -0,0 +1,77 @@
+const { nanoid } = require("nanoid");
+const books = require("./books");
+
+const addBookHandler = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const id = nanoid(16);
+  const finished = pageCount === readPage;
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  books.push(newBook);
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+  if (!name) {
+    const response = h.response({
+      status: "fail",
+      message: "Failed to add the book. Please fill in the book name",
+    });
+
+    response.code(400);
+    return response;
+  } else if (readPage > pageCount) {
+    const response = h.response({
+      status: "fail",
+      message:
+        "Failed to add the book. readPage cannot be greater than pageCount",
+    });
+
+    response.code(400);
+    return response;
+  } else if (isSuccess) {
+    const response = h.response({
+      status: "success",
+      message: "Book added successfully",
+      data: { bookId: id },
+    });
+
+    response.code(201);
+    return response;
+  } else {
+    const response = h.response({
+      status: "fail",
+      message: "Failed to add the book",
+    });
+
+    response.code(400);
+    return response;
+  }
+};
+
+module.exports = { addBookHandler };
